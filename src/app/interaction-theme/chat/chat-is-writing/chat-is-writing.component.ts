38b30c9d5c58536408c00b98692687a8810cbc68
Federico Moretti <fmoretti@vivocha.com>
2024-08-27 11:11:50 +0200

    fix: add support for agent avatar and nickname in is-writing messages

--- src/app/interaction-theme/chat/chat-is-writing/chat-is-writing.component.ts
+++ src/app/interaction-theme/chat/chat-is-writing/chat-is-writing.component.ts
@@ -6,7 +6,7 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChatIsWritingComponent{
-
+  @Input() agent;
   @Input() context;
 
 }
